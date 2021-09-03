42a23eb45784c3ad833e4b82758882c996a2a225
shafiqXikmal <mohdshafiqikmalmuh@gmail.com>
2021-09-03 20:14:49 +0800

    Adding Files

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+const MovieSearch = require("./movieSearch");
+
+function App() {
+  return (
+    <div>
+      <h1>Movie Search App</h1>
+      <MovieSearch />
+    </div>
+  );
+}
+
+module.exports = App;
